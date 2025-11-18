736d5bc9786a0ce2ff9b7a879f77a3f1316c1b45
emason2012 <emason20@student.cscc.edu>
2025-11-18 16:21:13 -0500

    P2- Created Alien displayed on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,6 +10,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from arsenal import Arsenal
+from Alien import Alien
 
 class AlienInvasion:
     #controls base functions of the game such as display, sounds, and fps
@@ -35,6 +36,7 @@ class AlienInvasion:
         self.laser_sound.set_volume(0.7)
 
         self.ship = Ship(self, Arsenal(self))
+        self.alien = Alien(self, 10,10)
     
     
     def run_game(self) -> None:
@@ -42,6 +44,7 @@ class AlienInvasion:
         while self.running:
             self._check_events()
             self.ship.update()
+            self.alien.update()
             self._update_screen()
             self.clock.tick(self.settings.FPS)
 
@@ -49,6 +52,7 @@ class AlienInvasion:
         #updates the screen for ship location
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
+        self.alien.draw_alien()
         pygame.display.flip()
 
     def _check_events(self) -> None:
