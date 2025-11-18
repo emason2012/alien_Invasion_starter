6ce469d6e69bd8d0b67600d957f2fe5f1819556a
emason2012 <emason20@student.cscc.edu>
2025-11-18 17:29:01 -0500

    created row of stationary aliens

--- Alien.py
+++ Alien.py
@@ -10,15 +10,15 @@ from pygame.sprite import Sprite
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from alien_invasion import AlienInvasion
+    from alien_fleet import AlienFleet
 
 class Alien(Sprite):
-    def __init__(self, game: 'AlienInvasion', x = float, y = float):
+    def __init__(self, fleet: 'AlienFleet', x = float, y = float):
         super().__init__()
         
-        self.screen = game.screen
-        self.boundries = game.screen.get_rect()
-        self.settings = game.settings
+        self.screen = fleet.game.screen
+        self.boundries = fleet.game.screen.get_rect()
+        self.settings = fleet.game.settings
 
         self.image = pygame.image.load(self.settings.alien_file)
         self.image = pygame.transform.scale(self.image, 
@@ -37,7 +37,7 @@ class Alien(Sprite):
 
         if self.check_edges():
             self.settings.fleet_direction *= -1
-            self.y += self.settings.fleet_dropspeed
+            self.y += self.settings.fleet_drop_speed
         
         self.x += temp_speed * self.settings.fleet_direction
         self.rect.x = self.x
--- settings.py
+++ settings.py
@@ -33,4 +33,4 @@ class Settings:
         self.alien_h = 40
         self.fleet_speed = 5
         self.fleet_direction = 1
-        self.fleet_dropspeed = 40
+        self.fleet_drop_speed = 40
