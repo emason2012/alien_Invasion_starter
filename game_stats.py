f8e06afa85f7d00cbeba46ed73f93f7f78ec3f75
emason2012 <emason20@student.cscc.edu>
2025-11-27 23:44:00 -0500

    Added the ability to track score as well as high score.

--- game_stats.py
+++ game_stats.py
@@ -4,6 +4,8 @@ Ethan Mason
 11/18
 This will store all of the data for the game and all stats it tracks
 """
+#from pathlib import Path
+import json
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -15,7 +17,28 @@ class GameStats():
         self.game = game
         self.settings = game.settings
         self.max_score = 0
-        self.reset_stats() 
+        self.init_saved_scores()
+        self.reset_stats()
+
+    def init_saved_scores(self):
+        self.path = self.settings.scores_file
+        if self.path.exists() and self.path.stat.__sizeof__() > 80:
+            contents = self.path.read_text()
+            scores = json.loads(contents)
+            self.hi_score = scores.get('hi_score', 0)
+        else:
+            self.hi_score = 0
+            self.save_scores()
+
+    def save_scores(self):
+        scores = {
+            'hi_score': self.hi_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f'File Not Found: {e}')
 
     def reset_stats(self):
         self.ships_left = self.settings.starting_ship_count
@@ -27,9 +50,15 @@ class GameStats():
 
         self._update_max_score()
 
+        self._update_hi_score()
+
     def _update_max_score(self):
         if self.score > self.max_score:
             self.max_score = self.score
+            
+    def _update_mhi_score(self):
+        if self.score > self.hi_score:
+            self.hi_score = self.score
 
     def _update_score(self, collisions):
         for alien in collisions.values():
